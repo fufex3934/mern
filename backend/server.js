43ac748f97c8c297035bce30ab33d4117c3f7128
fufex3934 <fufawakgari174@gmail.com>
2025-03-12 15:53:28 +0300

    update products

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,15 @@ const app = express();
 // Middleware to parse JSON request body
 app.use(express.json());
 
-// Route to add a new product
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.json({ success: true, data: products });
+  } catch (error) {
+    console.log("Error fetching products: ", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
 app.post("/api/products", async (req, res) => {
   const product = req.body;
 
@@ -30,6 +38,30 @@ app.post("/api/products", async (req, res) => {
   }
 });
 
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    await Product.findByIdAndDelete(id);
+    res.json({ succes: true, message: "Product deleted" });
+  } catch (error) {
+    console.log("Error deleting product: ", error.message);
+    res.status(404).json({ success: false, message: "Product not Found" });
+  }
+});
+
+app.put("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = req.body;
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, product, {
+      new: true,
+    });
+    res.status(200).json({ success: true, data: updatedProduct });
+  } catch (error) {
+    console.log("Error updating products", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+});
 // Start the server
 const startServer = async () => {
   await connectDB();
