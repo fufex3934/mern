328743864ceac19b39bbf6f0bf7cbdea5beb6554
fufex3934 <fufawakgari174@gmail.com>
2025-03-12 16:16:34 +0300

    adding controllers

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,65 +1,19 @@
-import express from 'express';
-import Product from '../models/product.model.js';
+import express from "express";
+import Product from "../models/product.model.js";
+import {
+  getProducts,
+  createProduct,
+  deleteProduct,
+  updateProduct,
+} from "../controllers/product.controller.js";
 const router = express.Router();
 
-router.get('/',async (req, res) => {
-    try {
-      const products = await Product.find({});
-      res.json({ success: true, data: products });
-    } catch (error) {
-      console.log("Error fetching products: ", error.message);
-      res.status(500).json({ success: false, message: "Server Error" });
-    }
-  });
+router.get("/", getProducts);
 
-  router.post('/',async (req, res) => {
-    const product = req.body;
-  
-    if (!product.name || !product.price || !product.image) {
-      return res.status(400).json({
-        success: false,
-        message: "Please provide all required fields",
-      });
-    }
-  
-    try {
-      const newProduct = new Product(product);
-      await newProduct.save();
-      res.status(201).json({ success: true, data: newProduct });
-    } catch (error) {
-      console.log("Error in creating new Product:", error.message);
-      res.status(500).json({ success: false, message: "Server Error" });
-    }
-  });
+router.post("/", createProduct);
 
-  router.delete("/:id", async (req, res) => {
-    const { id } = req.params;
-    try {
-      await Product.findByIdAndDelete(id);
-      res.json({ succes: true, message: "Product deleted" });
-    } catch (error) {
-      console.log("Error deleting product: ", error.message);
-      res.status(404).json({ success: false, message: "Product not Found" });
-    }
-  });
+router.delete("/:id", deleteProduct);
 
-  router.put("/:id",async (req, res) => {
-    const { id } = req.params;
-    const product = req.body;
-    if (!mongoose.Types.ObjectId.isValid(id)) {
-      return res
-        .status(404)
-        .json({ success: false, message: "Product not found" });
-    }
-    try {
-      const updatedProduct = await Product.findByIdAndUpdate(id, product, {
-        new: true,
-      });
-      res.status(200).json({ success: true, data: updatedProduct });
-    } catch (error) {
-      console.log("Error updating products", error.message);
-      res.status(500).json({ success: false, message: "Server Error" });
-    }
-  });
+router.put("/:id", updateProduct);
 
 export default router;
