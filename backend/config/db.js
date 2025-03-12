6c3635977a938c45a63dfd5380259e5437b4046f
fufex3934 <fufawakgari174@gmail.com>
2025-03-12 11:12:59 +0300

    second commit

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,18 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv';
+dotenv.config();
+
 export const connectDB = async () => {
   try {
-    mongoose.connect(process.env.MONGODB_URI);
+    if (!process.env.MONGO_URI) {
+      throw new Error("MongoDB URI is undefined. Check .env file.");
+    }
+
+    await mongoose.connect(process.env.MONGO_URI,);
+
     console.log("MongoDB Connected");
   } catch (error) {
-    console.log(error);
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
   }
 };
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
-dotenv.config();
+dotenv.config();  
+import { connectDB } from './config/db.js';
+
 const app = express();
 
-app.get("/products",(req,res)=>{
-    res.send("server is ready");
-})
-
-console.log(process.env.MONGO_URI)
-app.listen(5000,()=>{
-    console.log("Server started at http://localhost:5000");
-})
+app.listen(process.env.PORT, async () => {
+    await connectDB();  
+    console.log(`Server started at http://localhost:${process.env.PORT}`);
+});
 //
